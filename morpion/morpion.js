ed4ce9fafdf305108ea9648e2a111c840dd53400
fares berrani <fberrani@hotmail.fr>
2022-12-15 11:38:47 +0100

    dataset js

---
+++ morpion/morpion.js
@@ -0,0 +1,30 @@
+var dataa = document.querySelectorAll('.case')
+console.log(dataa)
+
+
+
+for(let dataaa of dataa){
+    dataaa.addEventListener('click', check)
+}
+
+var x = "x"
+var o = 'o'
+
+var joueur1 = 'joueur1';
+var joueur2 = 'joueur2'; 
+
+function check(){
+ console.log(this.dataset.class)
+ if (joueur1 == ''){
+
+ }else if (this == ''){
+
+ }
+
+ }
+ 
+
+
+
+
+
