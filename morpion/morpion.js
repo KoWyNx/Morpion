9de390df9afe3d29271f4d601e3201011a3719aa
fares berrani <fberrani@hotmail.fr>
2022-12-15 12:30:02 +0100

    ajout css

--- morpion/morpion.js
+++ morpion/morpion.js
@@ -1,34 +1,22 @@
-var dataa = document.querySelectorAll('.case')
-console.log(dataa)
+var dataa = document.querySelectorAll(".case");
+console.log(dataa);
 
-
-
-for(let dataaa of dataa){
-    dataaa.addEventListener('click', check)
+for (let dataaa of dataa) {
+  dataaa.addEventListener("click", check);
 }
 
+var click = 0;
 
-var click = 0
+function check() {
+  console.log(this.dataset.class);
+  if (this.textContent == "") {
+    click++;
+    if (click % 2 == 0) {
+      this.textContent = "X";
+    } else {
+      this.textContent = "O";
+    }
+  } else {
+  }
 
-function check(){
-
- console.log(this.dataset.class)
-
-click++
-
- if (click % 2 == 0 ){
-    this.textContent = 'O'
-
- }else {
-    this.textContent = 'X'
- }
-
- }
-
- 
- 
-
-
-
-
-
+}
