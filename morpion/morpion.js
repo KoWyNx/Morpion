bf4f961460b98e567344a33685d354afb9b3702f
fares berrani <fberrani@hotmail.fr>
2022-12-15 16:29:12 +0100

    fin du morpion

--- morpion/morpion.js
+++ morpion/morpion.js
@@ -1,4 +1,9 @@
 var dataa = document.querySelectorAll(".case");
+var win = false;
+var joueur = document.querySelector('#joueur')
+
+
+var etat = ["", "", "", "", "", "", "", "", ""];
 console.log(dataa);
 
 for (let dataaa of dataa) {
@@ -12,11 +17,55 @@ function check() {
   if (this.textContent == "") {
     click++;
     if (click % 2 == 0) {
+        joueur.textContent = 'C\'est au tour du joueur1'
       this.textContent = "X";
+      etat.splice(this.dataset.class, 1, "X");
     } else {
+        joueur.textContent = 'C\'est au tour du joueur2'
       this.textContent = "O";
+      etat.splice(this.dataset.class, 1, "O");
     }
-  } else {
+  }
+  if (etat[0] == etat[1] && etat[2] == etat[1] && etat[0] != "") { 
+    joueur.textContent = etat[0] == 'X' ? 'C\'est le joueur1 qui à gagnez' : 'C\'est le joueur2 qui à gagnez'; 
+    win = true;
+  } else if (etat[3] == etat[4] && etat[5] == etat[4] && etat[3] != "") {
+    joueur.textContent = etat[0] == 'X' ? 'C\'est le joueur1 qui à gagnez' : 'C\'est le joueur2 qui à gagnez';
+    win = true;
+  } else if (etat[6] == etat[7] && etat[8] == etat[7] && etat[6] != "") {
+    joueur.textContent = etat[0] == 'X' ? 'C\'est le joueur1 qui à gagnez' : 'C\'est le joueur2 qui à gagnez';
+    win = true;
+  } else if (etat[0] == etat[3] && etat[6] == etat[3] && etat[0] != "") {
+    joueur.textContent = etat[0] == 'X' ? 'C\'est le joueur1 qui à gagnez' : 'C\'est le joueur2 qui à gagnez';
+    win = true;
+  } else if (etat[1] == etat[4] && etat[7] == etat[4] && etat[1] != "") {
+    joueur.textContent = etat[0] == 'X' ? 'C\'est le joueur1 qui à gagnez' : 'C\'est le joueur2 qui à gagnez';
+    win = true;
+  } else if (etat[2] == etat[5] && etat[8] == etat[5] && etat[2] != "") {
+    joueur.textContent = etat[0] == 'X' ? 'C\'est le joueur1 qui à gagnez' : 'C\'est le joueur2 qui à gagnez';
+    win = true;
+  } else if (etat[0] == etat[4] && etat[8] == etat[4] && etat[0] != "") {
+    joueur.textContent = etat[0] == 'X' ? 'C\'est le joueur1 qui à gagnez' : 'C\'est le joueur2 qui à gagnez';
+    win = true;
+  } else if (etat[2] == etat[4] && etat[6] == etat[4] && etat[2] != "") {
+    joueur.textContent = etat[0] == 'X' ? 'C\'est le joueur1 qui à gagnez' : 'C\'est le joueur2 qui à gagnez';
+    win = true;
+  } else if (!etat.includes("")) {
+    joueur.textContent = 'Match nul'
   }
 
+  if (win == true) {
+    for (let dataaa of dataa) {
+      dataaa.removeEventListener("click", check);
+    }
+  }
+}
+
+function restart() {
+  etat = ["", "", "", "", "", "", "", "", ""];
+  for (let dataaa of dataa) {
+    dataaa.textContent = "";
+    dataaa.addEventListener("click", check);
+  }
+  win = false
 }
